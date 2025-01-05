dd373d3f77f82486245b3deabfed13e3ffe4545e
Umaid Jawed <umaidjawed2002@gmail.com>
2025-01-05 17:15:38 +0530

    changes
    few changes made on the modal chekbox

--- src/modal-component/modal-component.js
+++ src/modal-component/modal-component.js
@@ -62,7 +62,7 @@ export function ModalExample() {
                   console.log("Selected Tables:", selectedTables);
                   closeModal();
                 }}
-                className="px-4  py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700"
+                className="px-4 confirmbooking py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700"
               >
                 Confirm Booking
               </button>
